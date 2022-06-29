8f3b657703cb76e05516d7a7bfa3a00433532e52
sunliguo <sunliguo2006@qq.com>
2022-06-29 20:59:26 +0800

    进一步完善

--- shell_script/backup_mysql_table.sh
+++ shell_script/backup_mysql_table.sh
@@ -3,27 +3,24 @@
 MYUSER="root"
 MYPASS="admin"
 HOST="localhost"
+BACKUPDIR="/mnt/TNAS_SUN"
 
-BACKUPDIR="/backup_mysql"
-
-DATE=`date -I`
+DATE=$(date -I)
 RETENTION=20
 #MYSQLDATA="/var/lib/mysql"
+
 [ ! -d $BACKUPDIR/$DATE ] && mkdir -p $BACKUPDIR/$DATE
 
 #DBS=`mysql -u$MYUSER -p$MYPASS -Bse "show databases"|grep -v "information_schema" |grep -v "test"`
 DBS='crawl'
-for db_name in $DBS
+for db_name in $DBS; do
 
-do
+  mkdir -p $BACKUPDIR/$DATE/$db_name
+  tables=$(mysql -u$MYUSER -p$MYPASS -Bse "use $db_name;show tables")
 
-mkdir -p $BACKUPDIR/$DATE/$db_name
-tables=`mysql -u$MYUSER -p$MYPASS -Bse "use $db_name;show tables"`
+  for table_name in $tables; do
+    echo $table_name
+    mysqldump -u$MYUSER -p$MYPASS -h$HOST $db_name $table_name >$BACKUPDIR/$DATE/$db_name/$table_name.sql
 
-for table_name in $tables
-do
-  echo $table_name
-mysqldump -u$MYUSER -p$MYPASS -h$HOST  $db_name $table_name >$BACKUPDIR/$DATE/$db_name/$table_name.sql
-
-done
+  done
 done
