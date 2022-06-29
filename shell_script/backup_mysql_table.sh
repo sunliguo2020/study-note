8d1b479af7cc2031c144ea5e5c8eb7d785ad7dd1
sunliguo <sunliguo2006@qq.com>
2022-06-29 17:42:06 +0800

    备份mysql中的每个表

---
+++ shell_script/backup_mysql_table.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MYUSER="root"
+MYPASS="admin"
+HOST="localhost"
+
+BACKUPDIR="/backup_mysql"
+
+DATE=`date -I`
+RETENTION=20
+#MYSQLDATA="/var/lib/mysql"
+[ ! -d $BACKUPDIR/$DATE ] && mkdir -p $BACKUPDIR/$DATE
+
+#DBS=`mysql -u$MYUSER -p$MYPASS -Bse "show databases"|grep -v "information_schema" |grep -v "test"`
+DBS='crawl'
+for db_name in $DBS
+
+do
+
+mkdir -p $BACKUPDIR/$DATE/$db_name
+tables=`mysql -u$MYUSER -p$MYPASS -Bse "use $db_name;show tables"`
+
+for table_name in $tables
+do
+  echo $table_name
+mysqldump -u$MYUSER -p$MYPASS -h$HOST  $db_name $table_name >$BACKUPDIR/$DATE/$db_name/$table_name.sql
+
+done
+done
